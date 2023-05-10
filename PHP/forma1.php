436c57d6c2435f338b3c9ffc626ecd79415f6212
eamarquezh <supericman@outlook.com>
2023-05-09 19:03:20 -0600

    forma 1

---
+++ PHP/forma1.php
@@ -0,0 +1,16 @@
+<?php
+
+$valA = $_POST['valA'];
+$valB = $_POST['valB'];
+
+$data = array();
+
+    $data['lasuma'] = $valA + $valB;
+    $data['laresta'] = $valA - $valB;
+    $data['lamultiplicacion'] = $valA * $valB;
+    $data['ladivision'] = $valA / $valB;
+
+
+    echo json_encode($data);
+
+?>
