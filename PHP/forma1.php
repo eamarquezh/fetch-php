c2680e40cafbae51f7391317a46b389d9b430d90
Eric Alfredo Marquez Hernandez <supericman@outlook.com>
2023-05-15 21:16:00 -0600

    Update forma1.php

--- PHP/forma1.php
+++ PHP/forma1.php
@@ -1,16 +1,10 @@
 <?php
-
 $valA = $_POST['valA'];
 $valB = $_POST['valB'];
-
 $data = array();
-
     $data['lasuma'] = $valA + $valB;
     $data['laresta'] = $valA - $valB;
     $data['lamultiplicacion'] = $valA * $valB;
     $data['ladivision'] = $valA / $valB;
-
-
     echo json_encode($data);
-
 ?>
